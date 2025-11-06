f7fff5ca4f80715ad329b896f33c47a3aee7da19
Sara Langis <lang0588@algonquinlive.com>
2025-11-06 10:39:40 -0500

    copied code from class

---
+++ Rome-Around/front-page.php
@@ -0,0 +1,7 @@
+<?php get_header();?>
+<p class="alert alert-danger">This is my front-page</p>
+<?php get_footer();?>
+<section class="hero-section">
+
+    <div></div>
+</section>
---
+++ Rome-Around/functions.php
@@ -0,0 +1,31 @@
+<?php 
+function mexico_style(){
+    wp_enqueue_style(
+        'mexico_primary_stylesheet', //slug
+        get_stylesheet_uri(), //gets the URL of the stylesheets
+        array(), //depedencies, none here
+        wp_get_theme()-> get('version'), //version, used for caches busting
+        'all' //media type
+    );
+}
+add_action('wp_enqueue_scripts', 'mexico_style');
+//add_action('hookName', 'functionName')
+
+function loading_bootstrap(){
+    wp_enqueue_style(
+        'bootstrap-css',
+        get_theme_file_uri('assets\bootstrap-4.1.3-dist\css\bootstrap.min.css'),
+        array(),
+        '',
+        'all'
+    );
+    wp_enqueue_script(
+        'bootstrap-js',
+        get_theme_file_uri('assets\bootstrap-4.1.3-dist\js\bootstrap.min.js'),
+        array('jquery'),
+        '',
+        true
+    );
+}
+
+add_action('wp_enqueue_scripts', 'loading_bootstrap');
