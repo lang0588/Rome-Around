14686afb88b19713ebe252d2d97a79d5e87a2135
Sara Langis <lang0588@algonquinlive.com>
2025-11-07 19:37:25 -0500

    added bootstrap & images

--- Rome-Around/functions.php
+++ Rome-Around/functions.php
@@ -1,14 +1,14 @@
 <?php 
 function mexico_style(){
     wp_enqueue_style(
-        'mexico_primary_stylesheet', //slug
+        'rome_primary_stylesheet', //slug
         get_stylesheet_uri(), //gets the URL of the stylesheets
         array(), //depedencies, none here
         wp_get_theme()-> get('version'), //version, used for caches busting
         'all' //media type
     );
 }
-add_action('wp_enqueue_scripts', 'mexico_style');
+add_action('wp_enqueue_scripts', 'rome_style');
 //add_action('hookName', 'functionName')
 
 function loading_bootstrap(){
