42d8e4837a588dd26c756d2a42293d6c9bc4e75b
Colin <seay0006@algonquinlive.com>
2025-11-08 13:05:29 -0500

    updating functions, adding comments to match new functions

--- functions.php
+++ functions.php
@@ -9,29 +9,36 @@ function rome_style(){
     );
 }
 add_action('wp_enqueue_scripts', 'rome_style');
+// add_action('hookName', 'functionName')
+
+// function allowing bootstrap in css/js
+function loading_bootstrap(){
+    wp_enqueue_style(
+        'bootstrap-css',
+        get_theme_file_uri('assets\bootstrap-4.1.3-dist\css\bootstrap.min.css'),
+        array(),
+        '',
+        'all'
+    );
+    wp_enqueue_script(
+        'bootstrap-js',
+        get_theme_file_uri('assets\bootstrap-4.1.3-dist\js\bootstrap.min.js'),
+        array('jquery'),
+        '',
+        true
+    );
+}
+    
+
 ?>
 
 
 
 
 
-// //add_action('hookName', 'functionName')
 
-// function loading_bootstrap(){
-//     wp_enqueue_style(
-//         'bootstrap-css',
-//         get_theme_file_uri('assets\bootstrap-4.1.3-dist\css\bootstrap.min.css'),
-//         array(),
-//         '',
-//         'all'
-//     );
-//     wp_enqueue_script(
-//         'bootstrap-js',
-//         get_theme_file_uri('assets\bootstrap-4.1.3-dist\js\bootstrap.min.js'),
-//         array('jquery'),
-//         '',
-//         true
-//     );
-// }
+
+
+
 
 // add_action('wp_enqueue_scripts', 'loading_bootstrap');
