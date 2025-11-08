fa38ae4ae6201af61eeb29b7e800d37a47753579
Colin <seay0006@algonquinlive.com>
2025-11-08 16:34:17 -0500

    updating navbar to functional state

--- header.php
+++ header.php
@@ -8,45 +8,43 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
+
     <!-- Bootstrap - Navbar -->
+<!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+    <a class="navbar-brand" href="<?php echo home_url(); ?>">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item <?php if(is_front_page()) echo 'active'; ?>">
+                <a class="nav-link" href="<?php echo home_url(); ?>">Home</a>
+            </li>
+            <li class="nav-item <?php if(is_page('attractions')) echo 'active'; ?>">
+                <a class="nav-link" href="<?php echo home_url('/attractions'); ?>">Attractions</a>
+            </li>
+            <li class="nav-item <?php if(is_page('events')) echo 'active'; ?>">
+                <a class="nav-link" href="<?php echo home_url('/events'); ?>">Events</a>
+            </li>
+            <li class="nav-item <?php if(is_page('hotels')) echo 'active'; ?>">
+                <a class="nav-link" href="<?php echo home_url('/hotels'); ?>">Hotels</a>
+            </li>
+        </ul>
+    </div>
 </nav>
-<h3> This is my header </h3>
 
-<h3>Test header</h3>
+<header id="site-header">
+    <div class="hero-banner" style="background-image: url('<?php echo get_theme_file_uri('assets\images\colosseum.webp'); ?>');">
+        <div class="hero-content">
+            <h1>Welcome to Our Site</h1>
+            <p>Discover our world of creativity and innovation.</p>
+        </div>
+    </div>
+</header>
+
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
