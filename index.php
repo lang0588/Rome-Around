a1a08d0ec5fb95f1c5fb449f935e15ef3d6d5e72
Colin <seay0006@algonquinlive.com>
2025-11-08 17:16:12 -0500

    adding index

--- index.php
+++ index.php
@@ -5,15 +5,8 @@
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-
   <html>
-
   <!-- Small Map Embed -->
  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d190028.35442690636!2d12.37119217293777!3d41.91020879175258!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132f6196f9928ebb%3A0xb90f770693656e38!2sRome%2C%20Metropolitan%20City%20of%20Rome%20Capital%2C%20Italy!5e0!3m2!1sen!2sca!4v1762434318730!5m2!1sen!2sca" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-
-
-
-
-
   </html>
   
