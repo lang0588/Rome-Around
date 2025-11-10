a16f7c0ff08db242fbc76664dff8ef390937a9d7
Colin <seay0006@algonquinlive.com>
2025-11-10 09:02:36 -0500

    removing testing fluf

--- front-page.php
+++ front-page.php
@@ -5,7 +5,7 @@
 
 <?php get_footer();?>
 <section class="hero-section">
-<img src="<?php echo get_theme_file_uri('assets\images\colosseum.webp'); ?>" alt="Example">
+
     <div></div>
 
     <h1>Hello World!</h1>
