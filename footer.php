69a57d05889eec28bea49f001411a1603bcad292
Colin <seay0006@algonquinlive.com>
2025-11-10 09:23:54 -0500

    updating the header and beginning work on the footer

--- footer.php
+++ footer.php
@@ -1,4 +1,78 @@
 
 <?php wp_footer();?>
+
+<?php
+// Define link arrays for each column
+$footer_links_about = [
+    ['title' => 'Home', 'url' => home_url()],
+    ['title' => 'Attractions', 'url' => home_url('/attractions')],
+    ['title' => 'Events', 'url' => home_url('/events')],
+    ['title' => 'Hotels', 'url' => home_url('/hotels')]
+];
+
+$footer_links_quick = [
+    ['title' => 'Rome Tours', 'url' => home_url('/tours')],
+    ['title' => 'Travel Tips', 'url' => home_url('/tips')],
+    ['title' => 'Blog', 'url' => home_url('/blog')],
+    ['title' => 'FAQ', 'url' => home_url('/faq')]
+];
+
+$footer_links_contact = [
+    ['title' => 'Contact Us', 'url' => home_url('/contact')],
+    ['title' => 'Support', 'url' => home_url('/support')],
+    ['title' => 'Privacy Policy', 'url' => home_url('/privacy')],
+    ['title' => 'Terms & Conditions', 'url' => home_url('/terms')]
+];
+?>
+
+<footer class="custom-footer py-5">
+    <div class="container text-center text-md-left">
+        <div class="row">
+
+            <!-- About Links -->
+            <div class="col-md-4 mb-4">
+                <h5>About</h5>
+                <ul class="list-unstyled">
+                    <?php foreach($footer_links_about as $link): ?>
+                        <li><a href="<?php echo esc_url($link['url']); ?>"><?php echo esc_html($link['title']); ?></a></li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+
+            <!-- Quick Links -->
+            <div class="col-md-4 mb-4">
+                <h5>Quick Links</h5>
+                <ul class="list-unstyled">
+                    <?php foreach($footer_links_quick as $link): ?>
+                        <li><a href="<?php echo esc_url($link['url']); ?>"><?php echo esc_html($link['title']); ?></a></li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+
+            <!-- Contact Links -->
+            <div class="col-md-4 mb-4">
+                <h5>Contact</h5>
+                <ul class="list-unstyled">
+                    <?php foreach($footer_links_contact as $link): ?>
+                        <li><a href="<?php echo esc_url($link['url']); ?>"><?php echo esc_html($link['title']); ?></a></li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+
+        </div>
+
+        <hr class="footer-divider">
+
+        <div class="row">
+            <div class="col-12 text-center">
+                <p class="mb-0">&copy; <?php echo date('Y'); ?> Rome Travel Planner. All rights reserved.</p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+
+
 </body>
 </html>
--- header.php
+++ header.php
@@ -8,17 +8,24 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
+<!-- Hero Banner -->
+<header id="hero-banner" class="text-center text-white d-flex align-items-center justify-content-center">
+    <div class="hero-content">
+        <h1>Welcome to Our Site</h1>
+        <p>Discover attractions, events, and hotels!</p>
+        <a href="<?php echo home_url('/attractions'); ?>" class="btn btn-primary btn-lg">Explore Now</a>
+    </div>
+</header>
 
     <!-- Bootstrap - Navbar -->
 <!-- Navbar -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="<?php echo home_url(); ?>">Navbar</a>
+<nav class="navbar navbar-expand-lg custom-navbar">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
+    <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+        <ul class="navbar-nav">
             <li class="nav-item <?php if(is_front_page()) echo 'active'; ?>">
                 <a class="nav-link" href="<?php echo home_url(); ?>">Home</a>
             </li>
@@ -35,15 +42,8 @@
     </div>
 </nav>
 
-<!-- Header -->
-<header id="site-header">
-    <div class="hero-banner" style="background-image: url('<?php echo get_theme_file_uri('assets/images/colosseum.webp'); ?>');">
-        <div class="hero-content">
-            <h1>Welcome to Our Site</h1>
-            <p>Discover our world of creativity and innovation.</p>
-        </div>
-    </div>
-</header>
+
+
 
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
