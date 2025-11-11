0c9b0f8497dcf2a720aecfa23c0151667e409376
Colin <seay0006@algonquinlive.com>
2025-11-11 08:13:59 -0500

    git pull

--- Rome-Around/front-page.php
+++ Rome-Around/front-page.php
@@ -1,5 +1,5 @@
 <?php get_header();?>
-<p class="alert alert-danger">This is my front-page</p>
+<p class="alert alert-danger">This is not my front-page</p>
 <?php get_footer();?>
 <section class="hero-section">
 
