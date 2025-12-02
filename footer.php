27de258ea1984f44d48d60a9830aaa00abef16d4
Colin <seay0006@algonquinlive.com>
2025-12-02 16:15:23 -0500

    fixing errors from others work, PULL FROM HERE ONLY

--- footer.php
+++ footer.php
@@ -8,19 +8,17 @@
 
 <footer class="footer">
     <div class="container">
-        <?php get_sidebar(); ?>
 
         <div class="row justify-content-center text-center">
             <!-- Footer Column 1 -->
             <div class="col-md-4 mb-3">
                 <h4 class="footer-heading">Menu 1</h4>
                 <?php
-                    wp_nav_menu(array(
-                        'theme_location' => 'footer-menu-1',
-                        'container' => false,
-                        'menu_class' => 'footer-links',
-                        'fallback_cb' => false
-                    ));
+                    wp_nav_menu([
+                        'theme_location'=> 'footer_nav_1',
+                        'menu_class' => 'footer-menu',
+                        'container' => 'nav'
+                    ]);
                 ?>
             </div>
 
@@ -28,12 +26,11 @@
             <div class="col-md-4 mb-3">
                 <h4 class="footer-heading">Menu 2</h4>
                 <?php
-                    wp_nav_menu(array(
-                        'theme_location' => 'footer-menu-2',
-                        'container' => false,
-                        'menu_class' => 'footer-links',
-                        'fallback_cb' => false
-                    ));
+                    wp_nav_menu([
+                        'theme_location'=> 'footer_nav_2',
+                        'menu_class' => 'footer-menu',
+                        'container' => 'nav'
+                    ]);
                 ?>
             </div>
 
@@ -41,12 +38,11 @@
             <div class="col-md-4 mb-3">
                 <h4 class="footer-heading">Menu 3</h4>
                 <?php
-                    wp_nav_menu(array(
-                        'theme_location' => 'footer-menu-3',
-                        'container' => false,
-                        'menu_class' => 'footer-links',
-                        'fallback_cb' => false
-                    ));
+                    wp_nav_menu([
+                        'theme_location'=> 'footer_nav_3',
+                        'menu_class' => 'footer-menu',
+                        'container' => 'nav',
+    ]);
                 ?>
             </div>
         </div>
@@ -57,28 +53,5 @@
         <p>&copy; <?php echo date('Y'); ?> Rome Travel Planner. All rights reserved.</p>
          
     </div>
-
-
-    <!-- MENU -->
-    <div>
-        <?php
-        //1
-        wp_nav_menu(array(
-            'theme_location'=> 'footer_nav_1',
-            'menu_class' => 'footer-menu',
-            'container' => 'nav'
-        ));
-        //2
-        wp_nav_menu(array(
-            'theme_location'=> 'footer_nav_2',
-            'menu_class' => 'footer-menu',
-            'container' => 'nav'
-        ));
-        //3
-        wp_nav_menu(array(
-            'theme_location'=> 'footer_nav_3',
-            'menu_class' => 'footer-menu',
-        ));
-        ?>
 
 </footer>
